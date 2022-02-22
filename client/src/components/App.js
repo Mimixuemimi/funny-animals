a7d47684a227a09c859ebaf65c38f7dace57c58e
Fengran Xue <FengranXue@Fengran75102666.fios-router.home>
2022-02-21 19:38:24 -0500

    added animalRouter, and it's working. need to change the name of migration createAnimalMeme, etc.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,28 +7,31 @@ import "../assets/scss/main.scss";
 import RegistrationForm from "./registration/RegistrationForm";
 import SignInForm from "./authentication/SignInForm";
 import TopBar from "./layout/TopBar";
+import SpeciesList from "./CategoriesList.js";
+import HomePage from "./HomePage";
 
 const App = (props) => {
   const [currentUser, setCurrentUser] = useState(null);
   const fetchCurrentUser = async () => {
     try {
-      const user = await getCurrentUser()
-      setCurrentUser(user)
-    } catch(err) {
-      setCurrentUser(null)
+      const user = await getCurrentUser();
+      setCurrentUser(user);
+    } catch (err) {
+      setCurrentUser(null);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchCurrentUser()
-  }, [])
+    fetchCurrentUser();
+  }, []);
 
   return (
     <Router>
       <TopBar user={currentUser} />
       <Switch>
         <Route exact path="/">
-          <h2>Hello from react</h2>
+          <h2>Welcome from App.js </h2>
+          <SpeciesList />
         </Route>
         <Route exact path="/users/new" component={RegistrationForm} />
         <Route exact path="/user-sessions/new" component={SignInForm} />
--- server/src/routes/rootRouter.js
+++ server/src/routes/rootRouter.js
@@ -2,9 +2,13 @@ import express from "express";
 import userSessionsRouter from "./api/v1/userSessionsRouter.js";
 import usersRouter from "./api/v1/usersRouter.js";
 import clientRouter from "./clientRouter.js";
+import animalsRouter from "./api/v1/animalsRouter.js";
+import { root } from "cheerio/lib/static.js";
+
 const rootRouter = new express.Router();
 rootRouter.use("/", clientRouter);
 
+rootRouter.use("/api/v1/animals", animalsRouter);
 rootRouter.use("/api/v1/user-sessions", userSessionsRouter);
 rootRouter.use("/api/v1/users", usersRouter); //place your server-side routes here
 
